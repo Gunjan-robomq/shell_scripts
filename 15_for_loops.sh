b751b5f97ecef92ee7c713f61875355063b7c561
Gunjan-robomq <gunjan.maheshwari@robomq.io>
2024-07-06 15:50:15 +0530

    adding 13, 14 ,15

---
+++ 15_for_loops.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#for loop
+
+for i in 1 2 3 4 5 
+do 
+	echo "Number is $i"
+done
+
+for name in AD GD DS
+do 
+	echo "name is $name"
+done
+
+for i in {1..10}
+do 
+	echo "Num is $i"
+done
+
